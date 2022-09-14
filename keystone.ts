9e0d54f174f515055cbbac48355c538594e408a2
Sami2105 <saministor21@gmail.com>
2022-09-14 03:22:13 +0300

    set up database and start with keystone

---
+++ keystone.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import{config, createSchema} from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-reciclare-team2';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 30,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+  },
+  lists: createSchema({
+    // to add
+    User
+  }),
+  ui: {
+    // change this for roles
+    isAccessAllowed: () => true,
+  },
+  // add sessions values
+});
